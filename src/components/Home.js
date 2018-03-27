088e7ef89da2efc18cd282dc3b0d35faefad7ee0
Tyler <tcameronsmith@gmail.com>
2018-03-27 21:44:43 +1100

    add base format for review tile

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import bangbang from '../assets/bangbang.jpg';
+import user from '../assets/user.jpg';
+import fakeData from '../fakeData/fakeData';
 
 class Home extends Component {
+    reviewDataTiles() {
+        const test = fakeData.map((review, index) => {
+            return (
+                <div className="tileContainer">
+                    <div className="tileHeader">
+                        <div className="tileUserIcon">
+                            <img src={user} alt="user" />
+                        </div>
+                        <div className="tileHeaderText">
+                            <h4 key={index}>{review.user} <span>@ {review.place}</span></h4>
+                            <p>{review.date}</p>
+                        </div>
+                    </div>
+                    <p className="tileDescrip">{review.description}</p>
+                    <div className="tileImageContainer">
+                        <img src={bangbang} alt="bangbang" />
+                    </div>
+                </div>
+            );
+        });
+        // return <h1>{fakeData[0].user}</h1>;
+        // console.log(fakeData);
+        return test;
+    }
     render() {
         return (
             <div>
                 <h1>Home Page</h1>
                 <img src={bangbang} alt="bangbang" />
+                {this.reviewDataTiles()}
             </div>
         )
     }
