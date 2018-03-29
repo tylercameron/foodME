2bbaf60f5a876003f510f4c509c3503eb7691e82
Tyler <tcameronsmith@gmail.com>
2018-03-29 20:57:54 +1100

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,79 @@ import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StyledLink = styled(NavLink) `
-    border-radius: 3px;
-    padding: 0.25em 1em;
-    margin: 0 1em;
-    background: transparent;
-    color: palevioletred;
-    border: 2px solid palevioletred;
-    text-decoration: none;
 
-    &.selected {
-        color: blue;
+
+class Header extends Component {
+    renderContent() {
+        // switch (this.props.auth) {
+            // case null:
+            //     return;
+            // case false:
+            //     return <li><a href="/auth/google">Login With Google</a></li>;
+            // default:
+                return [
+                    <ListItem key="1"><StyledLink to='/' exact activeClassName="selected">Home</StyledLink>{' '}</ListItem>,
+                    <ListItem key="2"><StyledLink to='/about' activeClassName="selected">About</StyledLink>{' '}</ListItem>,
+                    <ListItem key="3"><StyledLink to='/map' activeClassName="selected">Map</StyledLink></ListItem>                    
+                ]
+        // }
+    }
+
+    render() {
+        return (
+            <Container>
+                <Link to='/' className="brand-logo"><h1 style={{ margin: 0 }}>Logo</h1></Link>
+                <Nav>
+                    <Ul>
+                        {this.renderContent()}
+                    </Ul>
+                </Nav>
+            </Container>
+        );
+    }
+}
+
+export default (Header);
+
+const Container = styled.header`
+    background: #ff5a5a;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 0 10px;
+    padding: 15px 30px;
+`;
+
+const ListItem = styled.li`
+    border-right: 1px solid #9d0000;
+    padding: 0.25em 1em;
+    
+    display: flex;
+    align-items: center;
+
+    &:last-child {
+        padding-right: 0;
+        border-right: none;
     }
 `;
 
-const Ul = styled.ul `
+const StyledLink = styled(NavLink)`
+    margin: 0;
+    background: transparent;
+    color: white;
+    text-decoration: none;
+
+    transition: color 0.1s ease-in;
+
+    &.selected {
+        color: #9d0000;
+    }
+
+    &:hover {
+        color: #9d0000;
+    }
+`;
+
+const Ul = styled.ul`
     list-style-type: none;
     padding-left: 0;
     display: flex;
@@ -28,44 +86,3 @@ const Nav = styled.nav`
     display: flex;
     justify-content: space-between;
 `;
-
-class Header extends Component {
-    renderContent() {
-        // switch (this.props.auth) {
-            // case null:
-            //     return;
-            // case false:
-            //     return <li><a href="/auth/google">Login With Google</a></li>;
-            // default:
-                return [
-                    <li key="1"><StyledLink to='/' exact activeClassName="selected">Home</StyledLink>{' '}</li>,
-                    <li key="2"><StyledLink to='/about' activeClassName="selected">About</StyledLink>{' '}</li>,
-                    <li key="3"><StyledLink to='/map' activeClassName="selected">Map</StyledLink></li>                    
-                ]
-        // }
-    }
-
-    render() {
-        return (
-            <Nav>
-                <Link to='/' className="brand-logo">Logo</Link>
-                <Ul>
-                    {this.renderContent()}
-                </Ul>
-            </Nav>
-        );
-    }
-}
-
-// a.selected {
-//     background: red;
-//     color: white;
-// }
-
-export default (Header);
-
-// <nav>
-//     <StyledLink to='/' exact activeClassName="selected">Home</StyledLink>{' '}
-//     <StyledLink to='/about' activeClassName="selected">About</StyledLink>{' '}
-//     <StyledLink to='/map' activeClassName="selected">Map</StyledLink>
-// </nav>
